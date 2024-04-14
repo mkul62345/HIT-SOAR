bd9d79b71a49c5f91755a0beca6a50883287dfc2
mkul62345 <maximkul@my.hit.ac.il>
2024-04-14 19:31:47 +0300

    Fixed tests

--- SOAR/tests.py
+++ SOAR/tests.py
@@ -1,31 +1,43 @@
 import requests
 
 def ping_server():
-    url = "http://localhost:5000/block"
-    response = requests.get(url=url)
-    if response.status_code == 200:
-        print("Server up")
-        return 1
-    print("Server down")
-    return 0
-
+    try:
+        url = "http://localhost:5000/block"
+        response = requests.get(url=url)
+        if response.status_code == 200:
+            print("Server up")
+            return 1
+        raise Exception("Server down")
+    
+    except Exception as e:
+        print("Server down")
+        return 0
+    
 def check_auth():
-    url = "http://localhost:5000/system_screen"
-    response = requests.get(url=url)
-    if response.status_code == 401:
-        print("Auth up")
-        return 1
-    print("Auth down")
-    return 0
-
+    try:
+        url = "http://localhost:5000/system_screen"
+        response = requests.get(url=url)
+        if response.status_code == 401:
+            print("Auth up")
+            return 1
+        raise Exception("Auth down")
+    
+    except Exception as e:
+        print("Auth down")
+        return 0
+    
 def ping_db():
-    url = "http://localhost:5000/dbstatus"
-    response = requests.get(url=url)
-    if response.status_code == 200:
-        print("DB up")
-        return 1
-    print("DB down")
-    return 0
+    try:
+        url = "http://localhost:5000/dbstatus"
+        response = requests.get(url=url)
+        if response.status_code == 200:
+            print("DB up")
+            return 1
+        raise Exception("DB down")
+    
+    except Exception as e:
+        print("DB down")
+        return 0
 
 def run_tests():
     sum = 0
